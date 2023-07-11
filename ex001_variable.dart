3f1fc81b82f52b7399ebaa507f27efb977d6d848
Daniel de castro <102565588+estudentDaniel@users.noreply.github.com>
2023-07-11 00:07:36 -0300

    Initial commit

---
+++ ex001_variable.dart
@@ -0,0 +1,19 @@
+void main(){
+  //Declarar variavel
+  var name = 'Daniel'; //o valor pode ser alterado
+  const nickname = 'castro';// o valor é permanente
+  final finalname = 'peres';//deve ser definida apenas uma vez
+
+  //--------------null-safety-----------------
+
+  //uma variavel nunca pode ser nula
+  String? nome;
+  int idade;
+  double medida;
+
+
+
+
+
+
+}
